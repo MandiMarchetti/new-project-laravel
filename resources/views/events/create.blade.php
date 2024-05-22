30dfd2193588d6b18683895cfb66d070fa7ae9b3
Mandi Thomaz <118818675+MandiMarchetti@users.noreply.github.com>
2024-05-22 21:42:53 +0200

    "Updated create event form in create.blade.php: removed 'bg-white' class, added tags section with checkbox list"

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -19,7 +19,7 @@
                     })
             
                 }
-            }" enctype="multipart/form-data" class="p-4 bg-white dark:bg-slate-800 rounded-md">
+            }" enctype="multipart/form-data" class="p-4 dark:bg-slate-800 rounded-md">
                 @csrf
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
@@ -106,9 +106,19 @@
                         @enderror
                     </div>
                 </div>
-
-                <!-- TAGS -->
-
+                <div>
+                    <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">Tags</h3>
+                    <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                        <!-- FOREACH -->
+                        <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                            <div class="flex items-center pl-3">
+                                <input id="vue-checkbox-list" type="checkbox" name="...." value="....." class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="vue-checkbox-list" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">...</label>
+                            </div>
+                        </li>
+                        <!-- ENDFOREACH -->
+                    </ul>
+                </div>
                 <div>
                     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create</button>
                 </div>
